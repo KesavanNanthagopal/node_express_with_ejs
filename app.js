49ef47a08cd225fd28f460bb09177a59f92b939f
kesavannanthagopal <kesavannanthagopal2000@gmail.com>
2023-04-03 06:58:48 +0530

    ejs view engine rendering the file

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ const app = express();
 //listen in the 3000 port
 app.listen(3000);
 
+//set the EJS as the view engine
+app.set("view engine", "ejs"); //The ejs default render file which is present in the views folder
+// app.set("views", "MyViews"); If we want to change the default folder for the ejs
+
 //provide the response.
-
 app.get("/", (req, res) => {
-    res.send("<p>Hello this is </p>");
-  });
+  res.render("index");
+});
 
-  
 app.get("/about", (req, res) => {
-    res.send("<p>Hello this is about </p>");
-  });
+  res.render("about");
+});
 
-  //Redirect the page
+app.get("/new_blog", (req, res) => {
+  res.render("createBlog");
+});
+
+//Redirect the page
 app.get("/about-us", (req, res) => {
-    res.redirect('/about');
-  });
+  res.redirect("/about");
+});
 
-  
 app.use((req, res) => {
-    res.status(404).send("<p>404 Page not found</p>");
-  });
+  res.status(404).render("404");
+});
