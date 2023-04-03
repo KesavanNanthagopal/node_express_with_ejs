6d454c10bfb637da472ed380ec06f500bb3c2130
kesavannanthagopal <kesavannanthagopal2000@gmail.com>
2023-04-03 07:57:15 +0530

    partials are added header,nav,footer

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ app.set("view engine", "ejs"); //The ejs default render file which is present in
 // app.set("views", "MyViews"); If we want to change the default folder for the ejs
 
 //provide the response.
+const blog = [
+  { title: "one", description: "one is one" },
+  { title: "two", description: "two is two" },
+  { title: "three", description: "three is three" },
+];
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home", blog: blog });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/new_blog", (req, res) => {
-  res.render("createBlog");
+  res.render("createBlog", { title: "Create Blog" });
 });
 
 //Redirect the page
@@ -28,5 +33,5 @@ app.get("/about-us", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "File Not Found" });
 });
